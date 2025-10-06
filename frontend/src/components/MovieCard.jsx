8786720f2e8db7c292cc5cb8c4be47524ead00e4
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-10-06 20:14:56 +0300

    Update MovieCard.jsx

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -9,19 +9,21 @@ function MovieCard({ movie }) {
 
 
   return (
-    <div className='movie-card'>
-      <div className='movie-poster'>
+    <div className="movie-card">
+      <div className="movie-poster">
         <img src={movie.url} alt={movie.title} />
-        <div className='movie-overlay'></div>
-        <button className='favourite-btn' onClick={onFavoriteClick}>♥</button>
+        <div className="movie-overlay"></div>
+        <button className="favourite-btn" onClick={onFavoriteClick}>
+          ♥
+        </button>
       </div>
-      <div className='movie-info'> 
+      <div className="movie-info">
         <h3>{movie.title}</h3>
         <p>{movie.release_date}</p>
-
       </div>
     </div>
-  )
+  );
+  
 }
 
 export default MovieCard;
